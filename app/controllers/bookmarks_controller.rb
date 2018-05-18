15905416e91ef72f5d1f25cdf3d50639a3174d2e
Den Zubritskiy <katatsu12@gmail.com>
2018-05-19 00:57:27 +0300

    Add Search by title url

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,7 +2,7 @@ class BookmarksController < ApplicationController
   before_action :set_bookmark, only: [:show, :edit, :update, :destroy]
 
   def index
-    @bookmarks = Bookmark.all
+    @bookmarks = Bookmark.search(params[:search])
   end
 
   def show
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,9 @@
 class Bookmark < ApplicationRecord
+  def self.search(search)
+    if search
+      where(['title LIKE ?', "%#{search}%"])
+    else
+      all
+    end
+  end
 end
