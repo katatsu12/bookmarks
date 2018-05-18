bdea0c377f793fd6cf745b32f71864b3ad61dba5
Den Zubritskiy <katatsu12@gmail.com>
2018-05-18 23:39:37 +0300

    New model user and session controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :bookmarks
 
   root 'bookmarks#index'
+
+  match 'auth/:provider/callback', to: 'sessions#create', via: :all
+  match 'auth/failure', to: redirect('/'), via: :all
+  match 'singout', to: 'sessions#destroy', as: 'singout', via: :all
 end
